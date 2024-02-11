9f240a0f28098da8e08e20f8cebe49358bdce820
Alexander Dheins <alexanderdheins@hotmail.com>
2024-02-10 19:06:36 -0500

    add: primer codigo

---
+++ app/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by lexaqpdev.com
+ * User: AlexanderDheins
+ * Date: 10/02/2024
+ * Time: 6:22 PM
+ */
+
+ define('SERVIDOR', 'localhost');
+ define('USUARIO', 'root');
+ define('PASSWORD', '');
+ define('BD', 'sisgestionescolar');
+
+ define('APP_NAME','SISTEMA DE GESTION ESCOLAR');
+ define('APP_URL', 'http://localhost/sysgestionescolar/');
+ define('KEY_API_MAPS','');
