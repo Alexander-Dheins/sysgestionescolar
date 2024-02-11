caf146ef18443eb1a047150d9bb70ae57cebbc4d
Alexander Dheins <alexanderdheins@hotmail.com>
2024-02-10 19:29:00 -0500

    daa

--- app/config.php
+++ app/config.php
@@ -14,3 +14,14 @@
  define('APP_NAME','SISTEMA DE GESTION ESCOLAR');
  define('APP_URL', 'http://localhost/sysgestionescolar/');
  define('KEY_API_MAPS','');
+
+ $servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+
+ try{
+    $pdo = new PDO($servidor, USUARIO, PASSWORD,
+    array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAME utf8"));
+
+    echo "conexion exitosa a la base de datos";
+ }catch (PDOException $e){
+    echo "error de conexion con la db";
+ }
